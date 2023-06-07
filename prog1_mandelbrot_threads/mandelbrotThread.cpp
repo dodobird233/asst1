2a26a4132d522e4a62a592fa3a05b080a7288e79
LAPTOP-0F2EM23B\2901233546 <2901233546@qq.com>
2023-06-07 17:00:44 +0800

    update for task 1

--- prog1_mandelbrot_threads/mandelbrotThread.cpp
+++ prog1_mandelbrot_threads/mandelbrotThread.cpp
@@ -34,7 +34,11 @@ void workerThreadStart(WorkerArgs * const args) {
     // to compute a part of the output image.  For example, in a
     // program that uses two threads, thread 0 could compute the top
     // half of the image and thread 1 could compute the bottom half.
-
+    // for(int i=args->threadId;i<args->height;i+=args->numThreads){
+    //     mandelbrotSerial(args->x0,args->y0,args->x1,args->y1,args->width,args->height,i,1,args->maxIterations,args->output);
+    // }
+    int partheight=args->height/args->numThreads;
+    mandelbrotSerial(args->x0,args->y0,args->x1,args->y1,args->width,args->height,args->threadId*partheight,partheight,args->maxIterations,args->output);
     printf("Hello world from thread %d\n", args->threadId);
 }
 
